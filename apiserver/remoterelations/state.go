486bbf6db89be7dabeef96cd3089e07851451d1e
Andrew Wilkins <axwalk@gmail.com>
2015-11-24 17:26:30 +0800

    apiserver/remoterelations: expand API

--- apiserver/remoterelations/state.go
+++ apiserver/remoterelations/state.go
@@ -15,6 +15,12 @@ type RemoteRelationsState interface {
 	// be derived unambiguously from the relation's endpoints).
 	KeyRelation(string) (Relation, error)
 
+	// Relation returns the existing relation with the given id.
+	Relation(int) (Relation, error)
+
+	// RemoteService returns a remote service by name.
+	RemoteService(string) (RemoteService, error)
+
 	// WatchRemoteServices returns a StringsWatcher that notifies of changes to
 	// the lifecycles of the remote services in the environment.
 	WatchRemoteServices() state.StringsWatcher
@@ -27,11 +33,19 @@ type RemoteRelationsState interface {
 
 // Relation provides access a relation in global state.
 type Relation interface {
+	// Destroy ensures that the relation will be removed at some point; if
+	// no units are currently in scope, it will be removed immediately.
+	Destroy() error
+
 	// Id returns the integer internal relation key.
 	Id() int
 
 	// Life returns the relation's current life state.
 	Life() state.Life
+
+	// RemoteUnit returns a RelationUnit for the remote service unit
+	// with the supplied ID.
+	RemoteUnit(unitId string) (RelationUnit, error)
 
 	// Unit returns a RelationUnit for the unit with the supplied ID.
 	Unit(unitId string) (RelationUnit, error)
@@ -42,9 +56,21 @@ type Relation interface {
 	WatchCounterpartEndpointUnits(serviceName string) (state.RelationUnitsWatcher, error)
 }
 
-// RelationUnit provides access to the settings of a single unit in a relation.
+// RelationUnit provides access to the settings of a single unit in a relation,
+// and methods for modifying the unit's involvement in the relation.
 type RelationUnit interface {
+	EnterScope(settings map[string]interface{}) error
+	InScope() (bool, error)
+	LeaveScope() error
+	ReplaceSettings(map[string]interface{}) error
 	Settings() (map[string]interface{}, error)
+}
+
+// RemoteService represents the state of a service hosted in an external
+// (remote) environment.
+type RemoteService interface {
+	Name() string
+	URL() string
 }
 
 type stateShim struct {
@@ -59,8 +85,24 @@ func (st stateShim) KeyRelation(key string) (Relation, error) {
 	return relationShim{r, st.State}, nil
 }
 
+func (st stateShim) Relation(id int) (Relation, error) {
+	r, err := st.State.Relation(id)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return relationShim{r, st.State}, nil
+}
+
+func (st stateShim) RemoteService(name string) (RemoteService, error) {
+	s, err := st.State.RemoteService(name)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return remoteServiceShim{s}, nil
+}
+
 func (st stateShim) WatchRemoteServiceRelations(serviceName string) (state.StringsWatcher, error) {
-	s, err := st.RemoteService(serviceName)
+	s, err := st.State.RemoteService(serviceName)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
@@ -70,6 +112,14 @@ func (st stateShim) WatchRemoteServiceRelations(serviceName string) (state.Strin
 type relationShim struct {
 	*state.Relation
 	st *state.State
+}
+
+func (r relationShim) RemoteUnit(unitId string) (RelationUnit, error) {
+	ru, err := r.Relation.RemoteUnit(unitId)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return relationUnitShim{ru}, nil
 }
 
 func (r relationShim) Unit(unitId string) (RelationUnit, error) {
@@ -88,10 +138,30 @@ type relationUnitShim struct {
 	*state.RelationUnit
 }
 
+func (r relationUnitShim) ReplaceSettings(s map[string]interface{}) error {
+	settings, err := r.RelationUnit.Settings()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	settings.Update(s)
+	for _, key := range settings.Keys() {
+		if _, ok := s[key]; ok {
+			continue
+		}
+		settings.Delete(key)
+	}
+	_, err = settings.Write()
+	return errors.Trace(err)
+}
+
 func (r relationUnitShim) Settings() (map[string]interface{}, error) {
 	settings, err := r.RelationUnit.Settings()
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
 	return settings.Map(), nil
+}
+
+type remoteServiceShim struct {
+	*state.RemoteService
 }
